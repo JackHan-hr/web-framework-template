f2907fa64bf4df59df81625811644167ad87f231
HR <13982078994@139.com>
2018-07-25 11:51:52 +0800

    resolver

--- src/components/component.js
+++ src/components/component.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react';
 
+class Com extends Component {
+  render() {
+    return (
+      <div className="Com">
+        111
+      </div>
+    );
+  }
+}
+
+export default Com;
